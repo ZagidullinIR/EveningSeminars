b893a93ab59bc1852d25f0f2f77bf8c4e82410fe
Igor Zagidullin <Somors174@gmail.com>
2023-08-24 20:27:09 +0500

    Проверка работы

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -507,7 +507,7 @@ Counting (tutu);
 // y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 //b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
-
+/*
 Console.WriteLine("Введите переменную b1: ");
 double b1 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите переменную k1: ");
@@ -521,4 +521,6 @@ double x = (b2 - b1) / (k1 - k2);
 double y = x * k1 + b1;
 
 Console.Write($"Точка пересечения двух прямых: ( {x}; {y}).");
+*/
+
 
