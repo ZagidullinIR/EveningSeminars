027dd16beaa6d9fd7d3f4a37f8fd57c283a7e3df
Igor Zagidullin <Somors174@gmail.com>
2023-09-07 09:38:24 +0500

    Домашка 9 Семинар

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -987,10 +987,64 @@ Console.WriteLine(PrintNumber(1,8));
 // M = 1; N = 15 -> 120
 // M = 4; N = 8. -> 30
 
+/*
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+int temp = m;
 
+if (m > n)
+{
+    m = n;
+    n = temp;
+}
+
+PrintSum(m, n, temp = 0);
+
+void PrintSum(int m, int n, int sum)
+{
+    sum = sum + n;
+    if (n <= m)
+    {
+        Console.Write($"Сумма элементов = {sum} ");
+        return;
+    }
+    PrintSum(m, n - 1, sum);
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+*/
 
 
 // Задача 68: Напишите программу вычисления функции Аккермана 
 //с помощью рекурсии. Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 // m = 3, n = 2 -> A(m,n) = 29
+
+/*
+
+int m = InputNumbers("Введите M: ");
+int n = InputNumbers("Введите N: ");
+
+int funAkk = Akk(m, n);
+
+Console.Write($"Функция Аккермана = {funAkk} ");
+
+int Akk(int m, int n)
+{
+    if (m == 0) return n + 1;
+    else if (n == 0) return Akk(m - 1, 1);
+    else return Akk(m - 1, Akk(m, n - 1));
+}
+
+int InputNumbers(string input)
+{
+    Console.Write(input);
+    int output = Convert.ToInt32(Console.ReadLine());
+    return output;
+}
+*/
