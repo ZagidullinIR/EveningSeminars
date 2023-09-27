1c96deb90e4673872ffe3b1c305519709808b49c
Igor Zagidullin <Somors174@gmail.com>
2023-09-27 19:54:42 +0500

    proverka coda

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1048,3 +1048,18 @@ int InputNumbers(string input)
     return output;
 }
 */
+
+
+int sumNumbers(int x, int y)
+{
+    if (x == 0)
+        return y;
+    return sumNumbers(x - 1, y + 1);    
+}
+
+Console.Clear();
+Console.Write("Введите два числа");
+int[] numbers = Console.ReadLine()!.
+Split().Select(x => int.Parse(x)). ToArray();
+Console.WriteLine($"Результат {numbers[0]} + 
+{numbers[1]} = {sumNumbers(numbers[0], numbers[1])}");
